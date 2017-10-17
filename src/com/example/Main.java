c373ffc70d2d3e36e08a53b323107433a2997017
Qsior <lukaszbojes@gmail.com>
2017-10-17 19:19:52 +0200

    Added method overloading examples

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -3,6 +3,23 @@ package com.example;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int newScore = calculateScore("Tim", 500);
+        System.out.println("New score is " + newScore);
+        calculateScore(75);
+        calculateScore();
+    }
+
+    public static int calculateScore(String playerName, int score){
+        System.out.println("Player " + playerName + " scored " + score + " points");
+        return score*1000;
+    }
+
+    public static int calculateScore(int score){
+        System.out.println("Unknown player scored " + score + " points");
+        return score*1000;
+    }
+
+    public static void calculateScore(){
+        System.out.println("No player name, no score");
     }
 }
